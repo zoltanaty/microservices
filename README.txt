9867e0bfb53f29db535e4870ebffa7237fb8e836
Mate Zoltan <zoltanaty@gmail.com>
2018-06-10 23:39:10 +0300

    sor-szallitas-dronokkal docker configured, kubernetes configured, README.txt file added

---
+++ README.txt
@@ -0,0 +1,23 @@
+Microserveces, Docker, Kubernetes
+
+0. Login to docker via terminal: docker login
+
+1. Building the project and pushing the created Docker image to dockerhub:
+	run the sor-szallitas-dronokkal/docker/build.sh script
+
+2. Deploying on Kubernetes Cluster:
+	minikube start
+	kubectl run sor --image [dockerhub_user]/[image_name]:[tag] --port 8080
+	kubectl expose deployment sor --type=NodePort
+
+	checking the Kubernetes url: minikube service sor --url
+	delete the resources: kubectl delete all -l run=sor
+
+3. Starting Dashboard:
+	kubectl proxy
+	dashboard url: http://localhost:8001/api/v1/namespaces/kube-system/services/kubernetes-dashboard/proxy/#!/overview?namespace=default
+
+4. Scaling:
+	list your deployments: kubectl get deployments
+	scale: kubectl scale deployments/[deployment_name] --replicas=4
+	check that there are 4 pods now: kubectl get pods -o wide
--- sor-szallitas-dronokkal/docker/build.sh
+++ sor-szallitas-dronokkal/docker/build.sh
@@ -1,16 +1,29 @@
 #!/bin/bash
 
-docker build -t sor/gradle ./gradle || exit 1
+#deleting the previous generated docker images
+docker images | grep 'sor' | awk '{print $3}' | xargs docker rmi -f
 
+#creating an image for building the application
+docker build -t sor-gradle ./gradle || exit 1
+
+#building the application on the 'sor-gradle' container
 (
 cd ../
-docker run -t --rm -u root -v "${PWD}"://srv sor/gradle gradle clean || exit 1
-docker run -t --rm -u root -v "${PWD}"://srv sor/gradle gradle build || exit 1
+docker run -t --rm -u root -v "${PWD}"://srv sor-gradle gradle clean || exit 1
+docker run -t --rm -u root -v "${PWD}"://srv sor-gradle gradle build || exit 1
 )
 
+#copying the jar file to the 'docker/production' folder
 (
 cd ../
 cp build/libs/sor-szallitas-dronokkal*.jar docker/production/sor-szallitas-dronokkal.jar
 )
 
-docker build -t sor/production ./production || exit 1
+#creating the production image from the copied jar file
+docker build -t zoltanaty/sor-production ./production || exit 1
+
+#removing the jar file from the 'production' folder
+rm -rf production/*.jar
+
+#pushing the created image to dockerhub
+docker push zoltanaty/sor-production
