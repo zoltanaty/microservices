f25ec7c5aa06ea5690140575e22acc33538bda8e
zoltanaty <zoltanaty@gmail.com>
2018-06-10 15:48:47 +0300

    sor-szallitas-dronokkal docker configuration

--- sor-szallitas-dronokkal/docker/build.sh
+++ sor-szallitas-dronokkal/docker/build.sh
@@ -4,8 +4,13 @@ docker build -t sor/gradle ./gradle || exit 1
 
 (
 cd ../
-#docker run -t --rm -u root -v "${PWD}"://srv sor/gradle gradle clean || exit 1
+docker run -t --rm -u root -v "${PWD}"://srv sor/gradle gradle clean || exit 1
 docker run -t --rm -u root -v "${PWD}"://srv sor/gradle gradle build || exit 1
-ls
 )
 
+(
+cd ../
+cp build/libs/sor-szallitas-dronokkal*.jar docker/production/sor-szallitas-dronokkal.jar
+)
+
+docker build -t sor/production ./production || exit 1
